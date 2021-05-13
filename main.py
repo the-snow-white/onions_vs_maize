6763c2cdd487120d7c076c6a5c3ed43ab0692ab6
Humphryshikunzi <humphryshikunzi9@gmail.com>
2021-05-13 16:02:49 +0300

    Data Preprocessing

--- main.py
+++ main.py
@@ -8,73 +8,126 @@ import random
 import shutil
 import pathlib
 
-from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing.image import ImageDataGenerator, image_data_generator, load_img, img_to_array
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+#gpus = tf.config.experimental.list_physical_devices('CPU')
+#tf.config.experimental.set_memory_growth(gpus[0], True)
+
+#ImageDataGenerators
+rotation_range = 40
+width_shift_range = 0.2
+height_shift_range = 0.2
+shear_range = 0.2
+zoom_range = 0.2
+horizontal_flip = True
+fill_mode ='nearest'
+target_size = (150, 150)
+batch_size = 20
+class_mode = 'binary'
+
+#Model Parameters
+train_validation_split = 0.8
+metrics = ['accuracy']
+steps_per_epoch = 1000
+epochs = 50
+validation_steps = 500
+input_shape=(150, 150, 3)
 
 
 def create_folder_structure():
+    print('Creating folder structure')
     shutil.rmtree('./train')
-    pathlib.Path("./train/train/maize").mkdir(parents=True, exist_ok=True)
-    pathlib.Path("./train/train/onions").mkdir(parents=True, exist_ok=True)
-    pathlib.Path("./train/train/maize").mkdir(parents=True, exist_ok=True)
-    pathlib.Path("./train/validate/onions").mkdir(parents=True, exist_ok=True)
+    train_paths = ["./train/train/maize","./train/train/onions","./train/validate/maize", "./train/validate/onions"]
+    try:
+        for path in train_paths:
+            pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+            print('Created path  {}\n'.format(path))
+
+    except OSError as err:  
+        print(err)      
 
 
 def copy_images(source_list, destination_path):
+    print('Copying Images from {} to {}'.format(source_list, destination_path))
     for image in source_list:
         shutil.copyfile(f'./input_data/{image}', f'./train/{destination_path}/{image}')
+        print('Copied Image')
+        print(image)
 
 
-def create_training_and_validation_set(train_validation_split):
-    onions_and_maize_images = os.listdir('./input_data')
+def create_training_and_validation_set():
+    print('Creating training and validation data')
+    
+    onions_and_maize_images = os.listdir('./input_data')    
+    maize_images = list(filter(lambda image: 'Maize' in image, onions_and_maize_images))
+    onions_images = list(filter(lambda image: 'Onion' in image, onions_and_maize_images))
 
-    onions_images = list(filter(lambda image: 'onions' in image, onions_and_maize_images))
-    maize_images = list(filter(lambda image: 'maize' in image, onions_and_maize_images))
+    train_gen = ImageDataGenerator(
+        rotation_range = rotation_range,
+        width_shift_range = width_shift_range,
+        height_shift_range = height_shift_range,
+        shear_range = shear_range,
+        zoom_range = zoom_range,
+        horizontal_flip = horizontal_flip,
+        fill_mode = fill_mode
+    )
+    
+    #Load images 
+    maize_train_image = load_img('./input_data/{}'.format(maize_images[0]))
+    onion_train_image = load_img('./input_data/{}'.format(onions_images[0]))
+
+    # Create a numpy array with shape (3, 150, 150)
+    maize_train_image = img_to_array(maize_train_image)
+    onion_train_image = img_to_array(onion_train_image)
+
+    #Convert to Numpy array with shape (1, 3, 150, 150)
+    maize_train_image = maize_train_image.reshape((1,) + maize_train_image.shape)
+    onion_train_image = onion_train_image.reshape((1,) + onion_train_image.shape)
+
+    #Generate more data samples 
+    print('Generating more training data using Data Generator...')
+         
+
+    i = 1
+    for batch in train_gen.flow(maize_train_image, save_to_dir = "./input_data", save_prefix = 'Maize', save_format = 'jpg'):
+        i +=1
+        if i > 10:
+            break
+
+    for batch in train_gen.flow(onion_train_image, save_to_dir = "./input_data", save_prefix = 'Onion', save_format = 'jpg'):
+        i +=1
+        if i > 10:
+            break
+
+
+    onions_and_maize_images = os.listdir('./input_data')      
+    maize_images = list(filter(lambda image: 'Maize' in image, onions_and_maize_images))
+    onions_images = list(filter(lambda image: 'Onion' in image, onions_and_maize_images))
+
+    print(onions_and_maize_images)
+    print(onions_images)
+    print(maize_images)
 
     random.shuffle(onions_images)
     random.shuffle(maize_images)
 
     split_index = int(len(onions_images) * train_validation_split)
-
-    training_onions = onions_images[:split_index]
-    validation_onoins = onions_images[split_index:]
+    
     training_maize = maize_images[:split_index]
+    training_onions = onions_images[:split_index]    
     validation_maize = maize_images[split_index:]
+    validation_onions = onions_images[split_index:]
 
-    create_folder_structure()
-    copy_images(training_maize, 'train/maize')
-    copy_images(training_onions, 'train/onions')
-    copy_images(validation_maize, 'validate/maize')
-    copy_images(validation_onions, 'validate/onions')
+    file_paths = [training_maize, training_onions,validation_maize, validation_onions]
+    specific_path = ['train/maize', 'train/onions', 'validate/maize', 'validate/onions']
+
+    for i in range(4):
+        copy_images(file_paths[i], specific_path[i])
 
 
-def train_model():
-    train_gen = ImageDataGenerator(
-        rescale=1. / 255,
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest'
-    )
-
-    train_iterator = train_gen.flow_from_directory('./train/train',
-                                                   target_size=(150, 150),
-                                                   batch_size=20,
-                                                   class_mode='binary')
-
-    validation_gen = ImageDataGenerator(rescale=1. / 255.0)
-    validation_iterator = validation_gen.flow_from_directory('./train/validate',
-                                                             target_size=(150, 150),
-                                                             batch_size=10,
-                                                             class_mode='binary')
-
+def train_model(train_iterator, validation_iterator):
     model = keras.models.Sequential([
-        keras.layers.Conv2D(16, (3, 3), activation=tf.nn.relu, input_shape=(150, 150, 3)),
+        keras.layers.Conv2D(16, (3, 3), activation=tf.nn.relu, input_shape=input_shape),
         keras.layers.MaxPool2D((2, 2)),
         keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu),
         keras.layers.MaxPool2D((2, 2)),
@@ -87,13 +140,14 @@ def train_model():
 
     model.compile(optimizer=tf.optimizers.Adam(),
                   loss=tf.keras.losses.binary_crossentropy,
-                  metrics=['accuracy'])
+                  metrics=metrics)
 
+    print('Trainig the ML Model...')
     history = model.fit(train_iterator,
-                        validation_data=validation_iterator,
-                        steps_per_epoch=1000,
-                        epochs=50,
-                        validation_steps=500)
+                        validation_data = validation_iterator,
+                        steps_per_epoch = steps_per_epoch,
+                        epochs = epochs,
+                        validation_steps = validation_steps)
 
     model.save('maize-vs-onions.h5')
 
@@ -101,6 +155,7 @@ def train_model():
 
 
 def plot_result(history):
+    print('About to plot the results and graphs')
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
     epochs = range(len(acc))
@@ -122,6 +177,7 @@ def plot_result(history):
 
 
 def load_and_predict():
+    print('Loading the Model to perform inferences')
     model = keras.models.load_model('maize-vs-onions.h5')
 
     test_generator = ImageDataGenerator(rescale=1. / 255)
@@ -146,10 +202,20 @@ def load_and_predict():
     return predictions
 
 
-create_training_and_validation_set(0.8)
-result_history = train_model()
-plot_result(result_history)
-predictions = load_and_predict()
-df = pd.DataFrame(data=predictions, index=range(1, 12501), columns=['id', 'label'])
-df = df.set_index(['id'])
-df.to_csv('submission.csv')
+
+
+create_folder_structure()
+create_training_and_validation_set()
+
+#result_history = train_model()
+#result_history
+
+#plot_result(result_history)
+
+#predictions = load_and_predict()
+
+#df = pd.DataFrame(data=predictions, index=range(1, 12501), columns=['id', 'label'])
+#df = df.set_index(['id'])
+#df.to_csv('submission.csv')
+
+#create_folder_structure()
